402a09a5480325582f2653dd745d44bfbffb5564
vmhieu1211 <cavaldos1211@gmail.com>
2022-11-18 19:24:55 +0700

    11

--- route/web.php
+++ route/web.php
@@ -4,18 +4,10 @@
 if(!defined('APP_PATH')){
     die("cannot access");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// muc dich tiep nhan cac request tu phia client gui len
+// cac request do co the la cac phiong thuc : GET, POST ...
+// index.php?c=login&m=index
+// c: ten controller
+// m: phuong thuc nam trong controller
+// su dung ky thuat lazy loading (ten file va class giong nhau)
+// trong 1 file chi nen chua 1 class
