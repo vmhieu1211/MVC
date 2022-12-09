84d427e763ded6ae798b1d05b345f312a02904b7
vmhieu1211 <cavaldos1211@gmail.com>
2022-12-09 18:55:35 +0700

    11

--- admin/app/controller/RoleController.php
+++ admin/app/controller/RoleController.php
@@ -14,6 +14,15 @@ class RoleController extends Controller
         $this->roleModel = new Role();
     }
 
+    public function index()
+    {
+        return $this->loadView('roles/index_view');
+    }
+    public function create()
+    {
+        return $this->loadView('roles/create_view');
+    }
+
     public function store()
     {
         if(isset($_POST['save'])){
@@ -33,7 +42,7 @@ class RoleController extends Controller
             // neu co thi luu va thong bao loi
             if($this->roleModel->checkExistNameRole($nameRole)){
                 // nguoi dung nhap ten vai tro da co roi
-
+                return redirect('role','create');
             }  else{
                 //chua co ten vai tro trong db
                 $insert = $this->roleModel->insertRole($nameRole,$description);
